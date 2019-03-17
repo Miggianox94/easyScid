72fabaca6866ec5be26fec0f7a03762da6363d82
Salvatore <salvatore.coluccia94@gmail.com>
2019-03-17 18:59:57 +0100

    Initial commit

---
+++ easyscid-core/src/main/java/it/easyscid/coluccia/easyscidcore/EasyscidCoreApplication.java
@@ -0,0 +1,14 @@
+package it.easyscid.coluccia.easyscidcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyscidCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyscidCoreApplication.class, args);
+		
+	}
+
+}
